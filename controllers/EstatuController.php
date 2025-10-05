09d75b9a670f14e1dee89deb902e5e7b73725c46
kev <kkebcruz@gmail.com>
2025-10-05 05:20:22 -0600

    behaviors post

--- controllers/EstatuController.php
+++ controllers/EstatuController.php
@@ -1,9 +1,39 @@
 <?php
 namespace app\controllers;
 
+use yii\filters\Cors;
 use yii\rest\ActiveController;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
 
 class EstatuController extends ActiveController
 {
     public $modelClass = 'app\models\Estatu';
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        unset($behaviors['authenticator']);
+
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin'                           => ['http://localhost:8100','http://localhost:8101'],
+                'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'DELETE'],
+                'Access-Control-Request-Headers'   => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age'           => 600
+            ]
+        ];
+
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                HttpBearerAuth::className(),
+            ],
+            'except' => ['index', 'view']
+        ];
+
+        return $behaviors;
+    }
 }
--- controllers/ProveedorController.php
+++ controllers/ProveedorController.php
@@ -1,7 +1,10 @@
 <?php
 namespace app\controllers;
 
+use yii\filters\Cors;
 use yii\rest\ActiveController;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
 
 class ProveedorController extends ActiveController
 {
@@ -9,16 +12,28 @@ class ProveedorController extends ActiveController
     public function behaviors()
     {
         $behaviors = parent::behaviors();
+
+        unset($behaviors['authenticator']);
+
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::className(),
             'cors' => [
-                'Origin'                           => ['http://localhost:8100'],
-                'Access-Control-Request-Method'    => ['GET'],
+                'Origin'                           => ['http://localhost:8100','http://localhost:8101'],
+                'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'DELETE'],
                 'Access-Control-Request-Headers'   => ['*'],
                 'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Max-Age'           => 600
             ]
         ];
+
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                HttpBearerAuth::className(),
+            ],
+            'except' => ['index', 'view']
+        ];
+
         return $behaviors;
     }
 }
